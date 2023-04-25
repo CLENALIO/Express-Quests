dadf6d8a075d4f3a47cace134df88a5105100182
CLENALIO <caroline.le.nalio@gmail.com>
2023-04-25 11:25:07 +0200

    commit quete

--- app.js
+++ app.js
@@ -5,15 +5,15 @@ const app = express();
 const port = 5000;
 
 const welcome = (req, res) => {
-  res.send("Welcome to my favourite movie list");
+  res.send("Welcome to user list");
 };
 
 app.get("/", welcome);
 
-const movieHandlers = require("./movieHandlers");
+const userHandlers = require("./userHandlers");
 
-app.get("/api/movies", movieHandlers.getMovies);
-app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id", userHandlers.getUsersById);
 
 app.listen(port, (err) => {
   if (err) {
