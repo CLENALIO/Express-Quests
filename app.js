0d35957be055c22f22f7e3af0c45b484395276bd
CLENALIO <caroline.le.nalio@gmail.com>
2023-05-09 17:15:23 +0200

    quete 4bis

--- app.js
+++ app.js
@@ -6,21 +6,10 @@ app.use(express.json());
 const port = 5000;
 
 const welcome = (req, res) => {
-  res.send("Welcome to user list");
+  res.send("Welcome to the list");
 };
 
 app.get("/", welcome);
-
-const userHandlers = require("./userHandlers");
-
-app.get("/api/users", userHandlers.getUsers);
-app.get("/api/users/:id", userHandlers.getUsersById);
-
-app.post("/api/users", userHandlers.postUser);
-
-app.put("/api/users/:id", userHandlers.updateUser);
-
-app.delete("/api/users/:id", userHandlers.deleteUser);
 
 app.listen(port, (err) => {
   if (err) {
@@ -29,3 +18,30 @@ app.listen(port, (err) => {
     console.log(`Server is listening on ${port}`);
   }
 });
+
+const userHandlers = require("./userHandlers");
+const { validateUser } = require("./validators.js");
+
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id", userHandlers.getUsersById);
+
+app.post("/api/users", validateUser, userHandlers.postUser);
+
+app.put("/api/users/:id", validateUser, userHandlers.updateUser);
+
+app.delete("/api/users/:id", userHandlers.deleteUser);
+
+
+const movieHandlers = require("./movieHandlers");
+const { validateMovie } = require("./validators.js");
+
+app.get("/api/movies", movieHandlers.getMovies);
+app.get("/api/movies/:id", movieHandlers.getMovieById);
+
+app.post("/api/movies", validateMovie, movieHandlers.postMovie);
+
+app.put("/api/movies/:id", validateMovie, movieHandlers.updateMovie);
+
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
+
+
